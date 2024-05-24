68381c1af7dd6d7221a3477aad9fb03632dd003a
Mandresy ANDRIANTSOANAVALONA <13123849-mandresyandri@users.noreply.gitlab.com>
2024-05-24 12:03:30 +0200

    update algo fonctionnel

--- functions/filter.php
+++ functions/filter.php
@@ -2,31 +2,42 @@
 // FONTION DE FILTRE
 function executeSearch($pdo) {
         // Base de la requête SQL
-        $sql = "SELECT * FROM pretres_civiques_OBT WHERE 1=1";
-        $params = [];
-    
-        // "cite" filter
-        if (isset($_GET['cite']) && $_GET['cite'] != "all_cite") {
-            $sql .= " AND pretre_nom_cite = :cite";
-            $params[':cite'] = $_GET['cite'];
-        }
-    
-        // "pretrise" filter
-        if (isset($_GET['pretrise']) && $_GET['pretrise'] != "all_pretres") {
-            $sql .= " AND nom_pretrise = :pretre";
-            $params[':pretre'] = $_GET['pretrise'];
-        }
-    
-        // date range filter
-        if (isset($_GET['date_debut']) && isset($_GET['date_fin'])) {
-            $sql .= " AND datation_debut >= :debut AND datation_fin <= :fin";
-            $params[':debut'] = $_GET['date_debut'];
-            $params[':fin'] = $_GET['date_fin'];
-        }
-    
-        // search box filter
-        if (isset($_GET['search-box']) && $_GET['search-box'] != "") {
-            $sql .= " AND (
+        // Si empty search-box
+        if (empty($_GET["search-box"])) {
+            $sql = "SELECT * FROM pretres_civiques_obt WHERE 1=1";
+            $params = [];
+        
+            // "cite" filter
+            if (isset($_GET['cite']) && $_GET['cite'] != "all_cite") {
+                $sql .= " AND pretre_nom_cite = :cite";
+                $params[':cite'] = $_GET['cite'];
+            }
+        
+            // "pretrise" filter
+            if (isset($_GET['pretrise']) && $_GET['pretrise'] != "all_pretres") {
+                $sql .= " AND nom_pretrise = :pretre";
+                $params[':pretre'] = $_GET['pretrise'];
+            }
+        
+            // date range filter
+            if (isset($_GET['date_debut']) && $_GET['date_debut'] != "" && isset($_GET['date_fin']) && $_GET['date_fin'] != "") {
+                $sql .= " AND datation_debut >= :debut AND datation_fin <= :fin";
+                $params[':debut'] = $_GET['date_debut'];
+                $params[':fin'] = $_GET['date_fin'];
+            }
+        
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute($params);
+            $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        
+            // Afficher les résultats
+            return $results;
+        } 
+        
+        // Si search-box n'est pas empty
+        else {
+            $sql = "SELECT * FROM pretres_civiques_obt 
+            WHERE (
                 nom_grec LIKE :search 
                 OR nom_traduit LIKE :search 
                 OR nom_datation LIKE :search 
@@ -37,13 +48,14 @@ function executeSearch($pdo) {
                 OR contenu_traduit LIKE :search 
                 OR nom_corpus LIKE :search
             )";
+
             $params[':search'] = '%' . trim($_GET['search-box']) . '%';
+
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute($params);
+            $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        
+            // Afficher les résultats
+            return $results;
         }
-    
-        $stmt = $pdo->prepare($sql);
-        $stmt->execute($params);
-        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    
-        // Afficher les résultats
-        return $results;
     }
