0e162acb4ed770b08fa53cfd3de199616475a922
mandresy andri <70854158+mandresyandri@users.noreply.github.com>
2024-06-29 17:38:42 +0000

    redirect

--- includes/navbar.php
+++ includes/navbar.php
@@ -10,11 +10,13 @@
     <title><?php echo "Chercher un prêtre"?></title>
 </head>
 <body>
-    <navbar class="main-navbar">
-        <div href="https://pretres-civiques.mastercmw.com/"><img href="https://pretres-civiques.mastercmw.com/" src="https://pretres-civiques.mastercmw.com/wp-content/uploads/2023/06/logo-site.png" alt="logo" height="80px" weight="80px"> </div>
+<navbar class="main-navbar">
+        <div><img src="https://pretres-civiques.mastercmw.com/wp-content/uploads/2023/06/logo-site.png" 
+            alt="logo" height="80px" weight="80px"
+            onclick="window.location.href = 'https://pretres-civiques.mastercmw.com/';" style="cursor: pointer;"> </div>
         <div class="titre">
-        <span id="titre1" href="https://pretres-civiques.mastercmw.com/">Prêtres civiques</span> 
-        <span id="soustitre" href="https://pretres-civiques.mastercmw.com/">Les prêtres du culte impérial romain dans les cités de la province d'Asie</span> </div>
+        <span id="titre1" onclick="window.location.href = 'https://pretres-civiques.mastercmw.com/';" style="cursor: pointer;">Prêtres civiques</span> 
+        <span id="soustitre" onclick="window.location.href = 'https://pretres-civiques.mastercmw.com/';" style="cursor: pointer;">Les prêtres du culte impérial romain dans les cités de la province d'Asie</span> </div>
             <ul class="navbar">
                 <li><a href="../index.php">Recherche</a></li>
                 <li><a href="https://pretres-civiques.mastercmw.com/cites/">Cités</a></li> 
